135a112e6deb0db91affb0928c3f406ce1543b98
Olivier <olivier.winkler@icloud.com>
2024-05-10 09:50:58 +0200

    feat(styles): fix globals.css import

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,4 @@
-import "src/app/globals.css";
+import "@/app/globals.css";
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import Navbar from "@/components/navbar/Navbar";
